f6ef376cd5030893518d5d37050c41982b2412aa
Creotip <creotip@gmail.com>
2021-11-15 13:19:55 +0200

    fixes

--- apps/header/src/app/Header.tsx
+++ apps/header/src/app/Header.tsx
@@ -1,14 +1,11 @@
-import styled from '@emotion/styled';
-import { Box } from '@chakra-ui/react';
-
-const StyledApp = styled.div``;
+import { Box } from '@chakra-ui/react'
 
 export function Header() {
   return (
     <Box p={4} borderBottom="1px solid" borderColor="gray.100">
-      <h3>header app</h3>
+      <h3>header</h3>
     </Box>
-  );
+  )
 }
 
-export default Header;
+export default Header
